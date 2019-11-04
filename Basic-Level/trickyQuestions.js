e874da8ef7fb21dcbc8ce93678b04df9cd2f8f67
David <dhahn123@gmail.com>
2019-11-04 15:29:01 -0800

    use of call, slice in trickyQuestion

--- Basic-Level/trickyQuestions.js
+++ Basic-Level/trickyQuestions.js
@@ -14,3 +14,14 @@ var x = {
 
 x.method(y, 1);
 // 2 refers to amount of arguments passed inside the function and not the value of length
+
+
+// function returns total number of arguments
+// without use of loops
+let b  = function(){
+    return [].slice.call(arguments).length;
+    // call slice method on the arguments object (every fn has an object called arguments which is an array-like obj) which converts arguments into an array
+    // length will grab the amount of args inside the converted array
+};
+
+console.log(b(1,2,3,4,5))
