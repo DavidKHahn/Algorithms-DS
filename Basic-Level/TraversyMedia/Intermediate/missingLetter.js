ba72d9dc08c345d83f0c2962709c20e440f5859c
David <dhahn123@gmail.com>
2019-11-07 12:58:02 -0800

    add comments to missingLetter

--- Basic-Level/TraversyMedia/Intermediate/missingLetter.js
+++ Basic-Level/TraversyMedia/Intermediate/missingLetter.js
@@ -6,17 +6,23 @@
 
 function missingLetters(str) {
     let compare = str.charCodeAt(0);
+    // The charCodeAt() method returns an integer between 0 and 65535 representing the UTF-16 code unit at the given index.
     let missing;
+    // missing represents empty variable
 
     str.split('').map((char, i) => {
+    // separate string and return each character in array and map each index
         if(str.charCodeAt(i) == compare) {
+        // check if string charCode equals the initial first index (charCodeAt(0))
             ++compare;
+        // if so then increment to the next iteration
         } else {
             missing = String.fromCharCode(compare);
+        // otherwise set 'missing' to a String obj. using 'fromCharCode(compare)' which converts unicode values into characters from compare's current index.
         }
     });
-
     return missing;
+    // return missing to stop fn
 }
 
 console.log(missingLetters("abce")); // d
