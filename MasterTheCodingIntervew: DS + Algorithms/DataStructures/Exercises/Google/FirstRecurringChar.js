77d430f4c40832aff9dca9c4befb8a61fe8a50f4
David <dhahn123@gmail.com>
2020-01-12 17:28:27 -0800

    improved version for Google exercise

--- MasterTheCodingIntervew: DS + Algorithms/DataStructures/Exercises/Google/FirstRecurringChar.js
+++ MasterTheCodingIntervew: DS + Algorithms/DataStructures/Exercises/Google/FirstRecurringChar.js
@@ -3,14 +3,13 @@
 // Give an array = [2, 5, 1, 2, 3, 5, 1, 2, 4]:
 // It should return 2
 
-// Given an array = [2, 1, 1, 2, 3, 5, 1, 2, 4]:
+// Given an array = [1, 5, 1, 2, 3, 5, 1, 2, 4]:
 // It should return 1
 
 // Given an array = [2, 3, 4, 5]:
 // It should return undefined
 
 // Naive approach:
-
 function firstRecurringChar(input){
     for (let i = 0; i < input.length; i++) {
         // start at j = 1 to compare with 2nd index instead of the same first
@@ -22,6 +21,29 @@ function firstRecurringChar(input){
         }
     }
     return undefined;
-}
+} // O(n^2) since we have nested loops
 
-console.log(firstRecurringChar([2, 5, 1, 2, 3, 5, 1, 2, 4]));
+console.log(firstRecurringChar([2, 5, 1, 2, 3, 5, 1, 2, 4])); // 2
+console.log(firstRecurringChar([1, 5, 1, 2, 3, 5, 1, 2, 4])); // 1
+console.log(firstRecurringChar([2, 3, 4, 5])); // undefined
+
+// Improved version:
+function firstRecurringChar2(input){
+    // creating a new object which keeps track of all items in array
+    let map = {};
+    for (let i = 0; i < input.length; i++) {
+        // due to type coercion in JS we need to specify '!== undefined'
+        if(map[input[i]] !== undefined) {
+            return input[i]
+        } else {
+            map[input[i]] = i
+        }
+        console.log(map)
+    }
+    // return undefined in case none of the items match
+    return undefined;
+} // O(n)
+console.log(firstRecurringChar2([2, 5, 1, 2, 3, 5, 1, 2, 4])); // 2
+console.log(firstRecurringChar2([1, 5, 1, 2, 3, 5, 1, 2, 4])); // 1
+console.log(firstRecurringChar2([2, 3, 4, 5])); // undefined
+
