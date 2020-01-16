253f60d39290c9afce0041bac1bdf3e213f2f76f
David <dhahn123@gmail.com>
2020-01-15 20:38:37 -0800

    Insert method

---
+++ MasterTheCodingIntervew: DS + Algorithms/DataStructures/Trees/Exercises/BinaryTree.js
@@ -0,0 +1,70 @@
+class Node {
+    constructor(value){
+        this.left = null;
+        this.right = null;
+        this.value = value;
+    }
+}
+
+class BinarySearchTree {
+    constructor(){
+        this.root = null;
+    }
+    insert(value){
+        const newNode = new Node(value);
+        if (this.root === null) {
+        // make root node our newNode
+            this.root = newNode
+        } else {
+            let currentNode = this.root;
+            while(true) {
+            // if value is less than the currentNode's value go left
+                if(value < currentNode.value) {
+                    // left
+                    // if currentNode.left does not exist
+                    if(!currentNode.left) {
+                        // make newNode left
+                        currentNode.left = newNode;
+                        // return this to stop looping
+                        return this;
+                    }
+                    currentNode = currentNode.left
+                } else {
+                    // Right
+                    // if currentNode.right does not exist
+                    if(!currentNode.right) {
+                        currentNode.right = newNode;
+                        // return this to stop looping
+                        return this;
+                    }
+                    currentNode = currentNode.right;
+                }
+            }
+        }
+    }
+    lookup(value){
+    }
+    // remove
+}
+
+const tree = new BinarySearchTree();
+tree.insert(9);
+// tree.insert(4);
+// tree.insert(20);
+// tree.insert(170);
+// tree.insert(15);
+// tree.insert(1);
+console.log(JSON.stringify(traverse(tree.root))) // {"value":9,"left":null,"right":null}
+//      9
+//  4       20
+//1   6   15  170
+
+function traverse(node){
+    const tree = { value: node.value };
+    tree.left = { value: node.value };
+    tree.left = node.left === null ? null :
+    traverse(node.left);
+    tree.right = node.right === null ? null :
+    traverse(node.right);
+    return tree;
+}
