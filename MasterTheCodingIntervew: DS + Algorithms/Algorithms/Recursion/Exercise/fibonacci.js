58ec76a9ab2e0e407010dcaa414f54d7c581cc77
David <dhahn123@gmail.com>
2020-01-17 22:53:56 -0800

    Fibonacci Recursive, Iterative Exercise

--- MasterTheCodingIntervew: DS + Algorithms/Algorithms/Recursion/Exercise/fibonacci.js
+++ MasterTheCodingIntervew: DS + Algorithms/Algorithms/Recursion/Exercise/fibonacci.js
@@ -5,13 +5,30 @@
 
 // For example: fibonacciRecursive(6) should return 8
 
+// O(n): iterating through the loop
 function fibonacciIterative(n){
-
+    let arr = [0, 1];
+    for(let i = 2; i < n + 1; i++){
+        // adding the 2 previous values and pushing the value into arr
+        arr.push(arr[i-2] + arr[i-1])
+    }
+    return arr[n]
 }
-fibonacciIterative(3);
+console.log('fibonacciIterative', fibonacciIterative(3));
+console.log('fibonacciIterative', fibonacciIterative(8));
+console.log('fibonacciIterative', fibonacciIterative(5));
 
+
+// O(2^N) Exponential - recursive algorithms that solve a problem of N
 function fibonacciRecursive(n){
-
+// if n index is less than 2 return its current number
+    if (n < 2) {
+        return n
+    }
+    // the pattern is that each value is the sum of the 2 previous values
+    return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2)
 }
 
-fibonacciRecursive(3)
+console.log(fibonacciRecursive(6)) // 8
+console.log(fibonacciRecursive(8)) // 21
+console.log(fibonacciRecursive(1)) // 1
